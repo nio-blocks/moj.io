b1aa61ecd0fd1fd847bf798b1d3b215d6dfa1361
neutralioJenkins <jenkins@n.io>
2017-09-15 10:03:11 -0600

    Bump version to v2.0.1

--- mojio_vehicles_block.py
+++ mojio_vehicles_block.py
@@ -5,7 +5,7 @@ from nio.properties import VersionProperty
 class MojioVehicles(MojioBase):
 
     """ Notify details of connected moj.io vehicles """
-    version = VersionProperty("2.0.0")
+    version = VersionProperty("2.0.1")
 
     def _get_url_endpoint(self):
         return 'Vehicles'
